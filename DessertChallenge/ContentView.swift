22288c50678922d904bb167513a4fd157a504f1a
Jack Abraham <jtabraham@wisc.edu>
2024-05-21 13:58:52 -0500

    first commit

---
+++ DessertChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DessertChallenge
+//
+//  Created by Jack Abraham on 5/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DessertChallenge/DessertChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertChallengeApp.swift
+//  DessertChallenge
+//
+//  Created by Jack Abraham on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
